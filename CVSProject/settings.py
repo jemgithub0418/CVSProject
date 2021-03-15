ce3701e69f58c4b0cea917b15161f7c8eeafde84
jemgithub0418 <54935579+jemgithub0418@users.noreply.github.com>
2021-03-16 00:37:56 +0800

    Update settings.py

--- CVSProject/settings.py
+++ CVSProject/settings.py
@@ -149,8 +149,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'jemgaming0418@gmail.com'
-EMAIL_HOST_PASSWORD = 'jeremiramos'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 
 LOGIN_REDIRECT_URL = 'home'
